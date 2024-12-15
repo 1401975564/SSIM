f75e57ff82e68a3ee3b0850ddd64f84ad15922f1
1401975564 <85972398+1401975564@users.noreply.github.com>
2024-12-15 21:10:57 +0800

    Update vision_transformer.py

--- vision_transformer.py
+++ vision_transformer.py
@@ -118,7 +118,7 @@ class Block(nn.Module):
         return x
     
 class LSKblock(nn.Module):
-    def __init__(self, in_channels):  # 修改初始化参数为输入通道数
+    def __init__(self, in_channels):  
         super().__init__()
         self.conv0 = nn.Conv2d(in_channels, in_channels, 5, padding=2, groups=in_channels)
         self.conv_spatial = nn.Conv2d(in_channels, in_channels, 7, stride=1, padding=9, groups=in_channels, dilation=3)
@@ -155,7 +155,7 @@ class PatchEmbed(nn.Module):
 
         self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
         
-        self.lsk_block = LSKblock(in_chans)  # 修改LSKblock的初始化参数为输入通道数
+        self.lsk_block = LSKblock(in_chans)  
 
     def forward(self, x):
         B, C, H, W = x.shape
@@ -170,10 +170,8 @@ class PatchEmbed(nn.Module):
 
 def shift(x, gamma=1/12):
     B, C, H, W = x.shape
-#     print(x.shape)
     g = int(gamma * C)
     out = torch.zeros_like(x)
-    # 空间移位
     if g != 0:
         out[:, :, :, :-g] = x[:, :, :, g:]  # 左移
         out[:, :, :, -g:] = x[:, :, :, :g]  # 右移
@@ -181,8 +179,7 @@ def shift(x, gamma=1/12):
         out[:, :, -g:, :] = x[:, :, :g, :]  # 下移
     else:
         out = x
-    # 剩余通道
-    out[:, :, 4*g:, :] = x[:, :, 4*g:, :]  # 不移位
+    out[:, :, 4*g:, :] = x[:, :, 4*g:, :]  
     return out
 
 class VisionTransformer(nn.Module):
@@ -293,154 +290,11 @@ class VisionTransformer(nn.Module):
                 output.append(self.norm(x))
         return output
 
-class MaskedAutoencoderViT(nn.Module):
-    """ Vision Transformer """
-    def __init__(self, img_size=[224], patch_size=16, in_chans=3, num_classes=0, embed_dim=768, depth=12,
-                 num_heads=12, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
-                 drop_path_rate=0., norm_layer=nn.LayerNorm, **kwargs):
-        super().__init__()
-        self.num_features = self.embed_dim = embed_dim
-
-        self.patch_embed = PatchEmbed(
-            img_size=img_size[0], patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
-        num_patches = self.patch_embed.num_patches
-
-        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
-        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim))
-        self.pos_drop = nn.Dropout(p=drop_rate)
-
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
-        self.blocks = nn.ModuleList([
-            Block(
-                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
-            for i in range(depth)])
-        self.norm = norm_layer(embed_dim)
-
-        # Classifier head
-        self.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
-
-        trunc_normal_(self.pos_embed, std=.02)
-        trunc_normal_(self.cls_token, std=.02)
-        self.apply(self._init_weights)
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=.02)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-
-    def interpolate_pos_encoding(self, x, w, h):
-        npatch = x.shape[1] - 1
-        N = self.pos_embed.shape[1] - 1
-        if npatch == N and w == h:
-            return self.pos_embed
-        class_pos_embed = self.pos_embed[:, 0]
-        patch_pos_embed = self.pos_embed[:, 1:]
-        dim = x.shape[-1]
-        w0 = w // self.patch_embed.patch_size
-        h0 = h // self.patch_embed.patch_size
-        # we add a small number to avoid floating point error in the interpolation
-        # see discussion at https://github.com/facebookresearch/dino/issues/8
-        w0, h0 = w0 + 0.1, h0 + 0.1
-        patch_pos_embed = nn.functional.interpolate(
-            patch_pos_embed.reshape(1, int(math.sqrt(N)), int(math.sqrt(N)), dim).permute(0, 3, 1, 2),
-            scale_factor=(w0 / math.sqrt(N), h0 / math.sqrt(N)),
-            mode='bicubic',
-        )
-        assert int(w0) == patch_pos_embed.shape[-2] and int(h0) == patch_pos_embed.shape[-1]
-        patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
-        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
-
-    def prepare_tokens(self, x):
-        B, nc, w, h = x.shape
-        x = self.patch_embed(x)  # patch linear embedding
-
-        # add the [CLS] token to the embed patch tokens
-        cls_tokens = self.cls_token.expand(B, -1, -1)
-        x = torch.cat((cls_tokens, x), dim=1)
-
-        # add positional encoding to each token
-        x = x + self.interpolate_pos_encoding(x, w, h)
-
-        return self.pos_drop(x)
-    
-    def random_masking(self, x, mask_ratio):
-        """
-        Perform per-sample random masking by per-sample shuffling.
-        Per-sample shuffling is done by argsort random noise.
-        x: [N, L, D], sequence
-        """
-        N, L, D = x.shape  # batch, length, dim
-        len_keep = int(L * (1 - mask_ratio))
-
-        noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
-
-        # sort noise for each sample
-        ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
-        ids_restore = torch.argsort(ids_shuffle, dim=1)
-
-        # keep the first subset
-        ids_keep = ids_shuffle[:, :len_keep]
-        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
-
-        # generate the binary mask: 0 is keep, 1 is remove
-        mask = torch.ones([N, L], device=x.device)
-        mask[:, :len_keep] = 0
-        # unshuffle to get the binary mask
-        mask = torch.gather(mask, dim=1, index=ids_restore)
-
-        return x_masked, mask, ids_restore
-
-    def forward(self, x):
-        x = self.prepare_tokens(x)
-        x = x + self.pos_embed[:, :1, :]
-        x, mask, _ = self.random_masking(x, mask_ratio=0.01)  # 在这里调用random_masking方法
-        
-        # append cls token
-#         cls_token = self.cls_token + self.pos_embed[:, :1, :]
-#         cls_tokens = cls_token.expand(x.shape[0], -1, -1)
-#         x = torch.cat((cls_tokens, x), dim=1)
-        
-        for blk in self.blocks:
-            x = blk(x)
-        x = self.norm(x)
-        return x[:, 0]
-
-    def get_last_selfattention(self, x):
-        x = self.prepare_tokens(x)
-        for i, blk in enumerate(self.blocks):
-            if i < len(self.blocks) - 1:
-                x = blk(x)
-            else:
-                # return attention of the last block
-                return blk(x, return_attention=True)
-
-    def get_intermediate_layers(self, x, n=1):
-        x = self.prepare_tokens(x)
-        # we return the output tokens from the `n` last blocks
-        output = []
-        for i, blk in enumerate(self.blocks):
-            x = blk(x)
-            if len(self.blocks) - i <= n:
-                output.append(self.norm(x))
-        return output
-    
-    
 def vit_tiny(patch_size=16, **kwargs):
-    model = MaskedAutoencoderViT(
+    model = VisionTransformer(
         patch_size=patch_size, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4,
         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
-
-# def vit_tiny(patch_size=16, **kwargs):
-#     model = VisionTransformer(
-#         patch_size=patch_size, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4,
-#         qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
-#     return model
 
 
 def vit_small(patch_size=16, **kwargs):
